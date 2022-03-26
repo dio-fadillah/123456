06ad56996eb25cd48e41b8151e880974120af10b
dio-fadillah <dio.fadillah@gmail.com>
2022-03-26 19:39:24 +0700

    taskforce 26-mar

--- pages/signup.js
+++ pages/signup.js
@@ -1,44 +1,74 @@
 import Layout from "../components/Layout";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Button, Form, FormGroup, Label, Input, Col, Row } from 'reactstrap'
+import React, { useState } from "react";
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-const Index = () => {
+const Signup = () => {
+
+    const [inputValue, setInputValue] = useState({ email: '', password: '' })
+    const { email, password } = inputValue
+
+    const handleInput = e => {
+        const { name, value } = e.target
+        setInputValue(input => ({ ...input, [name]: value }))
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                if (user) {
+                    setInputValue({ email: '', password: '' })
+                    props.navigate('/login')
+                }
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log({ errorCode, errorMessage })
+            })
+        
+        
+    }
+
   return (
     <Layout pageTitle="Register Page">
       <Header />
 
       <main>
       <Container fluid>
-        <Row className="justify-content-md-center">
-            <Col md="auto">
-                <h1 className="display-4">PLAY TRADITIONAL GAME</h1>
-            </Col>
-        </Row>
-
-
-        <Row className="justify-content-md-center">
-            <Col md="auto">
-                <p className="lead">Experience new traditional game play</p>
-            </Col>
-        </Row>
-
-        <Row className="justify-content-md-center">
-            <Col md="auto">
-                <a className="btn btn-warning btn-lg" href="/login" role="button">PLAY NOW</a>
-            </Col>
-        </Row>
 
         <Row className="justify-content-md-center">
             <Col className="p-5" xs="4">
                 <Image src="/assets/home/01.jpg" alt="Picture of the author" width="350px" height="300px"/>
             </Col>
 
-            <Col className="p-5" xs="6">
-                <h2 className={styles.display_5}>Rock Paper Scissors</h2>
-                <p className={styles.display_10}>Rock paper scissors (also known by other orderings of the three items, with rock sometimes being called stone, or as Rochambeau, roshambo, or ro-sham-bo)  is a hand game, usually played between two people, in which each player simultaneously forms one of three shapes with an outstretched hand. These shapes are rock (a closed fist), paper (a flat hand), and scissors (a fist with the index finger and middle finger extended, forming a V). Scissors is identical to the two-fingered V sign (also indicating victory or peace) except that it is pointed horizontally instead of being held upright in the air.</p>
+            <Col className="p-5" xs="4">
+                <FormGroup className={styles.inner}>
+                    <Form>
+                        <h1>User Registration</h1>
+
+                        <FormGroup className="form-group">
+                            <Label className="label">Email</Label>
+                            <Input id='email' name='email' type='email' placeholder='Email' value={email} onChange={handleInput} />
+                        </FormGroup>
+
+                        <FormGroup className="form-group">
+                            <Label className="label">Password</Label>
+                            <Input id='password' name='password' type='password' placeholder='Password' value={password} onChange={handleInput} />
+                        </FormGroup>
+
+                        <Button onClick={handleSubmit} type="submit" className="btn btn-dark btn-lg btn-block">Register</Button>
+                        <p className="forgot-password text-right">
+                            Already registered? <a href="/login">Log in</a>
+                        </p>
+                    </Form>
+                </FormGroup>
             </Col>
         </Row>
       </Container>
@@ -48,5 +78,5 @@ const Index = () => {
     </Layout>
   );
 };
-export default Index;
+export default Signup;
 
