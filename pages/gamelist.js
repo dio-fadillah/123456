7da25bde9acc2b50aff58ec4fafc82d73e570447
dio-fadillah <dio.fadillah@gmail.com>
2022-04-06 17:13:03 +0700

    troubleshoot deploy

--- pages/gamelist.js
+++ pages/gamelist.js
@@ -1,6 +1,6 @@
 import Layout from "../components/Layout";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Header from "../components/header";
+import Footer from "../components/footer";
 import { Container, Row, Col } from 'reactstrap';
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
